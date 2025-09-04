e0c8eef3b12ecd082dcae4ddd57c53894c941b13
aiqyn <ajkynsahyntaj@gmail.com>
2025-09-04 16:06:19 +0500

    spring-sec

--- src/main/java/dev/mathalama/auth/config/SecurityConfig.java
+++ src/main/java/dev/mathalama/auth/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,10 +21,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
+            .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
-                .requestMatchers("/login", "/register", "/", "/css/**", "/js/**").permitAll()
+                .requestMatchers("/login", "/register", "/", "/css/**", "/js/**", "/assets").permitAll()
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form
